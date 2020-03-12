8f0c2470251083ce4ff4544f1691ada281cd6b1c
James Brown <jamesbbb1@gmail.com>
2020-03-12 14:19:00 -0500

    adding prototype

--- brown-assignment-starter.js
+++ brown-assignment-starter.js
@@ -8,7 +8,7 @@
 */
 
 // Require statement that imports the header.js file from my root directory.
-const header = require('../web-330/brown-header');
+const header = require('../brown-header');
 
 // Call the console.log() function and output a well-formatted header
 console.log(header.display('James', 'Brown', 'Exercise '));
